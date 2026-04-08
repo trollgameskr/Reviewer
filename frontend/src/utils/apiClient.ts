d2892c703ae96634d6734516616b1dd53a024679
chanseon.park <shinepcs@gmail.com>
2026-04-08 18:57:17 +0900

    나스에 ssh로 붙어서 설치 할때 에러 발생하는 부분들 수정함

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || '/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
